afe821c1770ec960957aaf74cfb662e161e77fc9
Aydin Terzi <aydinterzi7@gmail.com>
2023-09-15 23:54:57 +0300

    feat: profile page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,6 +1,8 @@
 import Button from '@/components/button'
+import ExploreFilter from '@/components/explore-filter'
 import HeaderImage from '@/components/header-image'
 import ProfileInfo from '@/components/profile-info'
+import Tweet from '@/components/tweet'
 import Image from 'next/image'
 import React from 'react'
 
@@ -17,7 +19,19 @@ function Profile({params} : ProfileProps) {
     <div>
       <div className='relative'>
         <HeaderImage/>
-        <div className='w-4/5 absolute -bottom-28 left-1/2 -translate-x-1/2'><ProfileInfo/></div>
+        <div className='w-full px-44 absolute -bottom-28 left-1/2 -translate-x-1/2'><ProfileInfo/></div>
+      </div>
+      <div className="flex gap-6 px-44 mt-40">
+      <div className="basis-1/4">
+      <ExploreFilter/>
+      </div>
+      <div className="basis-3/4 flex flex-col gap-14">
+      
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+        <Tweet/>
+      </div>
       </div>
      
     </div>
--- src/components/profile-info.tsx
+++ src/components/profile-info.tsx
@@ -5,19 +5,21 @@ import Image from 'next/image'
 function ProfileInfo() {
   return (
     <div className='flex p-6 rounded-lg shadow-md bg-white gap-6'>
-          <div className='basis-1/5'>
-            <Image src="./next.svg" width={150} height={150} alt='profile photo'/>
+          <div className='basis-1/5 relative'>
+            <div className='h-36 w-36 absolute -top-1/2'>
+                <Image src="./next.svg" fill alt='profile photo' className='  bg-blue-500 z-50'/>
+            </div>
           </div>
           <div className='basis-full'>
             <div className='flex justify-between'>
-              <div className='flex gap-6'>
-                <span>Daniel Jensen</span>
-                <span>2,569 Following</span>
-                <span> 10.8K Followers</span>
+              <div className='flex gap-6 text-gray-1 items-center'>
+                <span className='font-semibold  text-2xl'>Daniel Jensen</span>
+                <span className='text-xs font-semibold'>2,569 Following</span>
+                <span className='text-xs font-semibold'> 10.8K Followers</span>
               </div>
               <Button text='Follow'/>
             </div>
-            <p>Photographer & Filmmaker based in Copenhagen, <br /> Denmark ✵ 🇩🇰</p>
+            <p className='font-medium text-lg text-gray-3'>Photographer & Filmmaker based in Copenhagen, <br /> Denmark ✵ 🇩🇰</p>
           </div>
       </div>
   )
